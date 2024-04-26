860b443bbb6c0b33460f8c608139bd160a92c385
Paul Llanque <llanquepol@gmail.com>
2024-04-25 21:16:23 -0500

    [UPDATE] adding default config configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 import { ItemsModule } from './items/items.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
