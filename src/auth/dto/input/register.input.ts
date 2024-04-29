ad888e0b1dc8122066267e474d3460eade32bb76
Paul Llanque <llanquepol@gmail.com>
2024-04-28 19:18:13 -0500

    [ADD] auth input

---
+++ src/auth/dto/input/register.input.ts
@@ -0,0 +1,22 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+@InputType()
+export class RegisterInput {
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  fullName: string;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
