8341954503626fc3837f06b9c15f45e308fea299
Paul Llanque <llanquepol@gmail.com>
2024-04-28 19:22:04 -0500

    [UPDATE] create new user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
 import { User } from './entities/user.entity';
+import { RegisterInput } from 'src/auth/dto/input/register.input';
 
 @Injectable()
 export class UsersService {
+  private logger: Logger = new Logger('UsersService');
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(registerInput: RegisterInput): Promise<User> {
+    try {
+      const newUser = this.userRepository.create(registerInput);
+      return await this.userRepository.save(newUser);
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
   async findAll(): Promise<User[]> {
     return [];
   }
@@ -13,5 +39,13 @@ export class UsersService {
 
   async block(id: string): Promise<boolean> {
     return true;
+  }
+
+  private handleError(error: any): never {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    this.logger.error(error);
+    throw new InternalServerErrorException();
   }
 }
