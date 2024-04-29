1d814661eab062445be702fc273f78479237b3fa
Paul Llanque <llanquepol@gmail.com>
2024-04-28 20:41:39 -0500

    [ADD] config passportModule on defaultStrateegy, JwtModule on asyn with factory method

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthResolver } from './auth.resolver';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
 import { UsersModule } from 'src/users/users.module';
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
 
 @Module({
   providers: [AuthResolver, AuthService],
-  imports: [UsersModule],
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+        },
+      }),
+    }),
+    UsersModule,
+  ],
 })
 export class AuthModule {}
