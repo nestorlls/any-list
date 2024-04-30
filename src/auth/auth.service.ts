ac403953733961f515a78db1286e4e03bf6a1bbc
Paul Llanque <llanquepol@gmail.com>
2024-04-29 22:38:04 -0500

    [ADD] generate token and validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,27 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 import * as bcrypt from 'bcrypt';
 
 import { AuthResponse } from './types/auth-response.type';
 import { UsersService } from 'src/users/users.service';
 import { LoginInput, RegisterInput } from './dto/input';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async register(registerInput: RegisterInput): Promise<AuthResponse> {
     const user = await this.usersService.create(registerInput);
-    const token = 'token';
+    const token = this.getJwtToken(user.id);
     return { token, user };
   }
 
@@ -22,7 +31,21 @@ export class AuthService {
     if (!bcrypt.compareSync(password, user.password)) {
       throw new BadRequestException('Invalid credentials');
     }
-    const token = 'token';
+    const token = this.getJwtToken(user.id);
     return { token, user };
+  }
+
+  async validateUser(id: string): Promise<User> {
+    const user = await this.usersService.findOneById(id);
+    if (!user.isActive)
+      throw new UnauthorizedException(
+        'User is not active, try again or contact with an admin',
+      );
+    delete user.password;
+    return user;
+  }
+
+  private getJwtToken(userId: string): string {
+    return this.jwtService.sign({ id: userId });
   }
 }
