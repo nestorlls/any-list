ba30bfadb8942386d851ca7a645f5d6aef2715a6
Paul Llanque <llanquepol@gmail.com>
2024-04-29 22:44:23 -0500

    [ADD] revalidateToken endpoint

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,8 +1,11 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { AuthService } from './auth.service';
 import { AuthResponse } from './types/auth-response.type';
 import { LoginInput, RegisterInput } from './dto/input';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { User } from 'src/users/entities/user.entity';
 
 @Resolver()
 export class AuthResolver {
@@ -19,5 +22,12 @@ export class AuthResolver {
   @Mutation(() => AuthResponse, { name: 'login' })
   login(@Args('loginInput') loginInput: LoginInput): Promise<AuthResponse> {
     return this.authService.login(loginInput);
+  }
+
+  @Query(() => AuthResponse, { name: 'revalidate' })
+  @UseGuards(JwtAuthGuard)
+  revalidateToken(user: User): AuthResponse {
+    // return this.authService.revalidateToken();
+    throw new Error('Method not implemented.');
   }
 }
