c161ee24cf61524a11ed690957068fe2c15f2b1b
Paul Llanque <llanquepol@gmail.com>
2024-04-29 22:39:59 -0500

    [ADD] jwt strategy, jwt auth guard and interface payload

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { User } from 'src/users/entities/user.entity';
+import { IJwtPayload } from '../interfaces/jwt-payload.interface';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: IJwtPayload): Promise<User> {
+    const { id } = payload;
+    return await this.authService.validateUser(id);
+  }
+}
