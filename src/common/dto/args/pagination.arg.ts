ec30015a681ddc7f9ac96c051b59ab6d04c1ad85
Paul Llanque <llanquepol@gmail.com>
2024-05-13 17:51:42 -0500

    Refactor pagination arguments and add search argument for GraphQL DTOs

--- src/common/dto/args/pagination.arg.ts
+++ src/common/dto/args/pagination.arg.ts
@@ -1,17 +1,15 @@
 import { ArgsType, Field, Int } from '@nestjs/graphql';
-import { IsInt, IsOptional, IsPositive, Min } from 'class-validator';
+import { IsInt, IsOptional, Min } from 'class-validator';
 
 @ArgsType()
 export class PaginationArgs {
   @Field(() => Int, { nullable: true })
   @IsInt()
-  @IsPositive()
   @IsOptional()
   offset?: number = 0;
 
   @Field(() => Int, { nullable: true })
   @IsInt()
-  @IsPositive()
   @Min(1)
   @IsOptional()
   limit?: number = 10;
