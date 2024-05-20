0aecbf3b16982e99de0565e3e43af997b9734c51
Paul Llanque <llanquepol@gmail.com>
2024-05-19 21:01:51 -0500

    [ADD] add  relation field listItem

--- src/lists/entities/list.entity.ts
+++ src/lists/entities/list.entity.ts
@@ -1,10 +1,12 @@
 import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { ListItem } from 'src/list-item/entities/list-item.entity';
 import { User } from 'src/users/entities/user.entity';
 import {
   Column,
   Entity,
   Index,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -26,4 +28,8 @@ export class List {
   })
   @Index('userId_list_idx')
   user: User;
+
+  // @Field(() => [ListItem], { nullable: true })
+  @OneToMany(() => ListItem, (listItem) => listItem.list, { lazy: true })
+  listItem: ListItem[];
 }
